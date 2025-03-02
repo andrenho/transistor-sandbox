89c4ba2ff73a8523efb64a882e9bfc441ebf6c73
Andre Wagner <WagnerAndre@JohnDeere.com>
2025-03-01 20:32:03 -0600

    WIP

--- tests/serialization.cc
+++ tests/serialization.cc
@@ -9,15 +9,18 @@ extern "C" {
 #include "sandbox/sandbox.h"
 }
 
+#include "components.hh"
+
 TEST_SUITE("Serialization") {
 
     TEST_CASE("Serialization / unserialization")
     {
         ts_Sandbox sb;
         ts_sandbox_init(&sb);
+        ts_add_lua_components(&sb);
 
         ts_board_add_wire(&sb.boards[0], { 1, 1, TS_S }, { TS_WIRE_1, TS_LAYER_TOP });
-        ts_board_add_component(&sb.boards[0], "__vcc", { 2, 2, TS_CENTER }, TS_E);
+        ts_board_add_component(&sb.boards[0], "__led", { 2, 2, TS_CENTER }, TS_E);
 
         char serialized[4096];
         FILE* stream = fmemopen(serialized, sizeof(serialized), "w");
@@ -27,6 +30,7 @@ TEST_SUITE("Serialization") {
         printf("%s\n", serialized);
 
         ts_Sandbox sb2;
+        ts_add_lua_components(&sb2);
         ts_Result response = ts_sandbox_unserialize_from_string(&sb2, serialized);
         if (response != TS_OK)
             printf("%s\n", ts_last_error(&sb2, nullptr));
@@ -44,7 +48,7 @@ TEST_SUITE("Serialization") {
         CHECK(hmlen(sb2.boards[0].components) == 1);
         ts_Component* component = ts_board_component(&sb2.boards[0], { 2, 2, TS_CENTER });
         CHECK(component != nullptr);
-        CHECK(std::string(component->def->key) == "__vcc");
+        CHECK(std::string(component->def->key) == "__led");
         CHECK(component->direction == TS_E);
 
         ts_sandbox_finalize(&sb2);
