ebf568afdafaba5b374ea5b9e0bee70eeabc9677
Andre Wagner <WagnerAndre@JohnDeere.com>
2025-03-06 04:56:32 -0600

    .

--- tests/wrapper.cc
+++ tests/wrapper.cc
@@ -16,29 +16,29 @@ extern "C" {
 struct Fixture {
     Fixture(bool multithreaded)
     {
-        ts_transistor_init(&t, { multithreaded, multithreaded });
-        ts_transistor_component_db_add_from_lua(&t, button, -1);
-        ts_transistor_component_db_add_from_lua(&t, led, -1);
-        ts_transistor_component_db_add_from_lua(&t, or_2i, -1);
+        ts_init(&t, { multithreaded, multithreaded });
+        ts_component_db_add_from_lua(&t, button, -1);
+        ts_component_db_add_from_lua(&t, led, -1);
+        ts_component_db_add_from_lua(&t, or_2i, -1);
 
         // add wire
-        ts_transistor_cursor_set_pointer(&t, 0, { 1, 1 });
-        ts_transistor_cursor_key_press(&t, 0, 'w', 0);
-        ts_transistor_cursor_set_pointer(&t, 0, { 3, 1 });
-        ts_transistor_cursor_key_release(&t, 'w');
+        ts_on_cursor_set_position(&t, 0, { 1, 1 });
+        ts_on_cursor_key_press(&t, 0, 'w', 0);
+        ts_on_cursor_set_position(&t, 0, { 3, 1 });
+        ts_on_key_release(&t, 'w');
 
         // add components
-        ts_transistor_cursor_set_pointer(&t, 0, { 1, 1 });
-        ts_transistor_cursor_key_press(&t, 0, 'b', 0);
-        ts_transistor_cursor_set_pointer(&t, 0, { 3, 1 });
-        ts_transistor_cursor_key_press(&t, 0, 'l', 0);
+        ts_on_cursor_set_position(&t, 0, { 1, 1 });
+        ts_on_cursor_key_press(&t, 0, 'b', 0);
+        ts_on_cursor_set_position(&t, 0, { 3, 1 });
+        ts_on_cursor_key_press(&t, 0, 'l', 0);
 
-        ts_transistor_run(&t, 10000);
+        ts_run(&t, 10000);
     }
 
     ~Fixture()
     {
-        ts_transistor_finalize(&t);
+        ts_finalize(&t);
     }
 
     ts_Transistor t;
@@ -51,7 +51,7 @@ TEST_SUITE("Wrapper")
         Fixture f(false);
 
         ts_TransistorSnapshot snap;
-        ts_transistor_take_snapshot(&f.t, &snap);
+        ts_take_snapshot(&f.t, &snap);
 
         CHECK(snap.n_boards == 1);
         CHECK(snap.boards[0].n_components == 2);
@@ -68,19 +68,19 @@ TEST_SUITE("Wrapper")
         CHECK(std::string(button->def->key) == "__button");
         CHECK(button->data[0] == 0);
 
-        ts_transistor_cursor_set_pointer(&f.t, 0, { 1, 1 });
-        ts_transistor_cursor_click(&f.t, 0, TS_BUTTON_LEFT);
-        ts_transistor_run(&f.t, 10000);
+        ts_on_cursor_set_position(&f.t, 0, { 1, 1 });
+        ts_on_cursor_click(&f.t, 0, TS_BUTTON_LEFT);
+        ts_run(&f.t, 10000);
         CHECK(button->data[0] != 0);
 
         ts_TransistorSnapshot snap;
-        ts_transistor_take_snapshot(&f.t, &snap);
+        ts_take_snapshot(&f.t, &snap);
         CHECK(snap.boards[0].wires[0].value != 0);
         ts_snapshot_finalize(&snap);
 
-        ts_transistor_cursor_set_pointer(&f.t, 0, { 1, 1 });
-        ts_transistor_cursor_click(&f.t, 0, TS_BUTTON_LEFT);
-        ts_transistor_run(&f.t, 10000);
+        ts_on_cursor_set_position(&f.t, 0, { 1, 1 });
+        ts_on_cursor_click(&f.t, 0, TS_BUTTON_LEFT);
+        ts_run(&f.t, 10000);
         CHECK(button->data[0] == 0);
     }
 
@@ -88,30 +88,30 @@ TEST_SUITE("Wrapper")
     {
         Fixture f(true);
 
-        ts_transistor_lock(&f.t);
+        ts_lock(&f.t);
         ts_Component* button = f.t.sandbox.boards[0].components[0].value;
         CHECK(std::string(button->def->key) == "__button");
         CHECK(button->data[0] == 0);
-        ts_transistor_unlock(&f.t);
+        ts_unlock(&f.t);
 
-        ts_transistor_cursor_set_pointer(&f.t, 0, { 1, 1 });
-        ts_transistor_cursor_click(&f.t, 0, TS_BUTTON_LEFT);
+        ts_on_cursor_set_position(&f.t, 0, { 1, 1 });
+        ts_on_cursor_click(&f.t, 0, TS_BUTTON_LEFT);
         usleep(10000);
-        ts_transistor_lock(&f.t);
+        ts_lock(&f.t);
         CHECK(button->data[0] != 0);
-        ts_transistor_unlock(&f.t);
+        ts_unlock(&f.t);
 
         ts_TransistorSnapshot snap;
-        ts_transistor_take_snapshot(&f.t, &snap);
+        ts_take_snapshot(&f.t, &snap);
         CHECK(snap.boards[0].wires[0].value != 0);
         ts_snapshot_finalize(&snap);
 
-        ts_transistor_cursor_set_pointer(&f.t, 0, { 1, 1 });
-        ts_transistor_cursor_click(&f.t, 0, TS_BUTTON_LEFT);
+        ts_on_cursor_set_position(&f.t, 0, { 1, 1 });
+        ts_on_cursor_click(&f.t, 0, TS_BUTTON_LEFT);
         usleep(10000);
-        ts_transistor_lock(&f.t);
+        ts_lock(&f.t);
         CHECK(button->data[0] == 0);
-        ts_transistor_unlock(&f.t);
+        ts_unlock(&f.t);
 
         usleep(10000);
     }
